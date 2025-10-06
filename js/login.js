cf64c97eaa2d1e63cbe7ac3ac889d88ee44915f1
Angeles Pedersen <angieepedersen5@gmail.com>
2025-10-06 20:31:23 -0300

    Actualizacion general y agrego pagina de login

---
+++ js/login.js
@@ -0,0 +1,25 @@
+const menuButton = document.querySelector('.menu-button');
+const sidebar = document.querySelector('.sidebar');
+const overlay = document.querySelector('.overlay');
+
+if (menuButton && sidebar && overlay) {
+  menuButton.addEventListener('click', () => {
+    menuButton.classList.toggle('open');
+    sidebar.classList.toggle('active');
+    overlay.classList.toggle('active');
+  });
+
+  // Cierra menú al hacer clic en el fondo oscuro
+  overlay.addEventListener('click', () => {
+    menuButton.classList.remove('open');
+    sidebar.classList.remove('active');
+    overlay.classList.remove('active');
+  });
+}
+
+document.querySelector(".login-form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Inicio de sesión exitoso!")
+  window.location.href = "index.html";
+});
+
